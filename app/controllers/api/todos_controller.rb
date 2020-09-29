8c5da21444898f0ca792ea4cb55b200dc650d9a3
Henry Doan <henrydoan96@gmail.com>
2020-09-28 18:58:26 -0600

    rails side done

---
+++ app/controllers/api/todos_controller.rb
@@ -0,0 +1,46 @@
+class Api::TodosController < ApplicationController
+  # actions in the controller
+  # html
+  # xml 
+  # json - return object 
+  # Redirect to other actions that does
+  # no new, edit, html
+
+  def index
+    render json: Todo.all
+  end
+
+  # optional 
+  # def show
+  #   render json: Todo.find(params[:id])
+  # end
+
+  def create
+    todo = Todo.new(todo_params)
+    if todo.save
+      render json: todo
+    else
+      render json: { errors: todo.errors }, status: :unprocessable_entity
+    end 
+  end
+
+  def update
+    todo = Todo.find(params[:id])
+    if todo.update(todo_params)
+      render json: todo
+    else
+      render json: { errors: todo.errors }, status: :unprocessable_entity
+    end 
+  end
+
+  def destroy
+    Todo.find(params[:id]).destroy
+    render json: { message: 'Todo deleted' }
+  end
+
+  private
+    def todo_params
+      params.require(:todo).permit(:title, :complete)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Api routes, api call will be defined
+  namespace :api do
+    resources :todos 
+
+    # resources :todos do
+    #   resources :comments
+    # end
+  end
 end
